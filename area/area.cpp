02afbe258554a7d8281e716b0a59e312aedb8806
gkm100 <grzegorzmalkiewicz@hotmail.com>
2015-10-15 08:37:31 +0200

    update

--- area/area.cpp
+++ area/area.cpp
@@ -1,9 +1,8 @@
 # include <iostream>
-# include <math.h>
+
 using namespace std;
 
 int main( ) 
-<<<<<<< HEAD
 { 
     float PI = 3.141592;                // variables can be initialized during declaration 
     double rad; 
@@ -17,12 +16,5 @@ int main( )
 			cout<<" R is smaller than 0 ";
 	}
     cout<< "\nArea of the circle is "<< PI * rad * rad<<endl; 
-=======
-{                 // variables can be initialized during declaration 
-    float  rad; 
-    cout<< "Enter the radius"; 
-    cin>>rad; 
-    cout<< "Area of the circle is "<<M_PI * rad * rad; 
->>>>>>> d2e5ada4662399bd921376713fe553e873d8b83c
     return 0;
 }  
