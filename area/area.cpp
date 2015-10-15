eee8c3061d6a2f49ea1bbaa31cf194f8ee067cc5
Borysiak <borys59@onet.eu>
2015-10-15 08:46:37 +0200

    Edytowalem program do liczenia promienia kola

--- area/area.cpp
+++ area/area.cpp
@@ -1,20 +1,19 @@
 # include <iostream>
-
+#include  <math.h>
 using namespace std;
 
 int main( ) 
-{ 
-    float PI = 3.141592;                // variables can be initialized during declaration 
+{              
     double rad; 
-    for(;;)
-	{
+    while(1)
+    {
 		cout<< "\nEnter the radius - "; 
 		cin>>rad; 
 		if(!(rad<0))
 			break;
 		else
 			cout<<" R is smaller than 0 ";
-	}
-    cout<< "\nArea of the circle is "<< PI * rad * rad<<endl; 
+    }
+    cout<< "\nArea of the circle is "<< M_PI * rad * rad<<endl; 
     return 0;
 }  
